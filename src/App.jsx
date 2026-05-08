a06699d71f749a33e159d713f58010f4be3f094e
Raahim <raahimrizwantcs@gmail.com>
2026-05-08 20:15:19 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    {/* <About/>
+    <Projects/>
+    <Contact/>
+    <Footer/> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    
+    <nav className='text-white'>
+        
+    </nav>
+  )
+}
+
+export default Navbar
